0446a11049cf65ee79185ba7200bdadbd963442b
vienpham2019 <vienpham2019@gmail.com>
2020-10-15 22:15:32 -0500

    add draw path method and backtrack path

--- src/Algorithms/self_solve.js
+++ src/Algorithms/self_solve.js
@@ -3,7 +3,7 @@ import {get_top_right_bottom_left} from './solve_maze/helper_method/algorithms_h
 
 let start_node , end_node , nodes , c , canvas , size 
 
-let current_node , current_path , myReq
+let current_node , current_path , myReq , find_path 
 
 const self_solve = props => {
     start_node = props.start_node
@@ -13,16 +13,23 @@ const self_solve = props => {
     c = props.c 
     canvas = props.canvas 
     size = props.size 
+    find_path = false 
 
     current_path = new Stack()
     current_path.push(`${current_node.x} , ${current_node.y}` , set_node(current_node))
-
-    run_solve_maze()
+    set_up()
 }
 
-const run_solve_maze = () => {
+const set_up = () => {
+    c.clearRect(0,0,canvas.width, canvas.height)
+    for(let node of nodes){
+        node.draw()
+    }
+    window.removeEventListener("keydown" , run_solve_maze)
+    window.addEventListener("keydown" , run_solve_maze)
+}
 
-    window.addEventListener("keydown" , e => {
+const run_solve_maze = e => {
         c.clearRect(0,0,canvas.width, canvas.height)
         current_node = current_path.peek()
         let {top , right , bottom , left} = get_top_right_bottom_left(current_node , nodes , size)
@@ -45,25 +52,42 @@ const run_solve_maze = () => {
         }
         for(let path of current_path.values()){
             path.star_size = size 
+            path.color = "LightSkyBlue"
+            current_path.peek().color = "MediumBlue"
             path.draw()
         }
-    })
+        if(find_path) {
+            window.removeEventListener("keydown" , run_solve_maze)
+            current_node = current_path.peek()
+            draw_finish_path()
+        }
+}
+
+const draw_finish_path = () => {
+    myReq = requestAnimationFrame(draw_finish_path)
+    current_node.color = "SpringGreen"
+    current_node.draw()
+    current_node = current_node.prev_node
+
+    if(!current_node) cancelAnimationFrame(myReq)
 }
 
 const find_next_step = (node , wall_number) => {
     if(node && !node.walls[wall_number]){
         let {x , y} = node 
         let key = '' + x + y 
+        if(end_node.x === x && end_node.y === y) find_path = true 
         if(!current_path.has(key)){
             current_path.push(key , set_node(node))
         }else{
             current_path.pop()
         }
     }
+
 }
 
 const set_node = (node) => {
-    let color = 'MediumBlue'
+    let color = "LightSkyBlue"
     let {x , y} = node 
     let new_node = new Block(x, y , c , size , color , current_path.peek())
     return new_node 
